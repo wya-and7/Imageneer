1c4d5e931189ccf99cedf064e413b547e75cf05b
aliviarizz <aliviariska0@gmail.com>
2026-05-12 10:40:33 +0700

    Menambah Dashboard

---
+++ app/src/main/java/com/project/imageneer/Dashboard.kt
@@ -0,0 +1,37 @@
+package com.project.imageneer
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.card.MaterialCardView
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnSolo = findViewById<Button>(R.id.btnSolo)
+        val btnMultiPlayer = findViewById<Button>(R.id.btnMultiplayer)
+
+        btnSolo.setOnClickListener {
+
+        }
+
+        btnMultiPlayer.setOnClickListener {
+
+        }
+    }
+}
